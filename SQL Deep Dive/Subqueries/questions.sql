af9360832f4cc3b8428aea0f844eb2229e8c9db7
Cihat Erol <87125050+cerol7891@users.noreply.github.com>
2022-03-30 15:06:24 +0300

    Update questions.sql

--- SQL Deep Dive/Subqueries/questions.sql
+++ SQL Deep Dive/Subqueries/questions.sql
@@ -5,10 +5,19 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
+SELECT *
+FROM orders
+join customers using(customerid)
+where state in ('OH','NY','OR')
 
+order by orderid
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Filter employees who have emp_no 110183 as a manager
 */
+select * 
+from employees
+join titles using(emp_no)
+where emp_no = 110183 and title='Manager'
